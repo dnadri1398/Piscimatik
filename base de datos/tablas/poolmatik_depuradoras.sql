7824f3d8db96364a60097a6fe52b512fd8b4e17b
saloje <48989456+saloje@users.noreply.github.com>
2019-03-27 13:17:42 +0100

    Add files via upload

---
+++ base de datos/tablas/poolmatik_depuradoras.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: poolmatik
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `depuradoras`
+--
+
+DROP TABLE IF EXISTS `depuradoras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `depuradoras` (
+  `idDepuradoras` int(11) NOT NULL AUTO_INCREMENT,
+  `fechaAlta` datetime(6) DEFAULT NULL,
+  `Usuario` varchar(45) NOT NULL,
+  `Direccion` varchar(45) NOT NULL,
+  `Telefono` int(9) NOT NULL,
+  `Ciudad` varchar(45) NOT NULL,
+  `Modelo` varchar(45) NOT NULL,
+  `Caudal` int(9) NOT NULL,
+  `Potencia` int(6) NOT NULL,
+  `Volumen` int(7) NOT NULL,
+  PRIMARY KEY (`idDepuradoras`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `depuradoras`
+--
+
+LOCK TABLES `depuradoras` WRITE;
+/*!40000 ALTER TABLE `depuradoras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `depuradoras` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-22 12:48:29
